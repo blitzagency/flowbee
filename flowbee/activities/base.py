f4bac96302f6becce6601b2012c862ee4093b029
Adam Venturella <aventurella@blitzagency.com>
2016-03-28 06:03:28 -0700

    feat: adds cancel_execution

--- flowbee/activities/base.py
+++ flowbee/activities/base.py
@@ -34,13 +34,63 @@ class Activities(object):
 
 class Workflow(object):
     @classmethod
+    def cancel_execution(cls, workflow_id, run_id, reason, details="", child_policy="TERMINATE"):
+        """Cancel Workflow Execution
+
+        :param workflow_id: the workflow_id returned from Workflow.start_execution
+        :param run_id: the run_id returned from Workflow.start_execution
+        :param reason: the reason for this termination
+        :param details: the details behind the reason (default "")
+        :param child_policy: one of TERMINATE|REQUEST_CANCEL|ABANDON. (default "TERMINATE")
+                             see docs for why you should consider REQUEST_CANCEL
+                             http://boto3.readthedocs.org/en/latest/reference/services/swf.html#SWF.Client.terminate_workflow_execution
+        :returns: None
+        """
+        domain = cls.domain
+        kwargs = {
+            "domain": domain,
+            "workflowId": workflow_id,
+            "runId": run_id,
+            "reason": reason,
+            "details": details,
+            "childPolicy": child_policy
+        }
+
+        client = utils.get_client()
+
+        try:
+            log.debug("Canceling workflow execution with: %s", kwargs)
+            response = client.terminate_workflow_execution(**kwargs)
+        except Exception as e:
+            log.error(
+                "Failed to cancel workflow '%s' with run_id '%s' "
+                "reason: %s",
+                workflow_id, run_id, e.message
+            )
+
+    @classmethod
     def start_execution(
         cls, input=None, version="0.0.1",
         child_policy="TERMINATE", priority=0, workflow_id=None,
         execution_start_to_close_timeout=60,  # "31536000 is 1 year
         task_start_to_close_timeout=10,
     ):
+        """Start workflow execution
 
+        :param input: the JSON serializable value to pass to the designated @entrypoint
+                      function (default None)
+        :param child_policy: one of TERMINATE|REQUEST_CANCEL|ABANDON. (default "TERMINATE")
+                             see docs for you should consider REQUEST_CANCEL
+                             http://boto3.readthedocs.org/en/latest/reference/services/swf.html#SWF.Client.start_workflow_execution
+        :param priority: valid values are integers that range from Java's Integer.MIN_VALUE (-2147483648) to
+                         Integer.MAX_VALUE (2147483647). Higher numbers indicate higher priority. (default 0)
+        :param workflow_id: workflow id for this workflow. If None a workflow id will be automatically
+                            generated for you. This value is retruned to you upon success weather passed in
+                            or generated automatically. (default None)
+        :param execution_start_to_close_timeout: How long can this execution take in total before it's timed out
+        :param task_start_to_close_timeout: How long before this task is resent to the decider
+        :returns: (workflow_id, run_id)
+        """
         domain = cls.domain
         tasklist = cls.tasklist
         workflow_type_version = version
